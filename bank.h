f02e90a2a161f794da53ef269c5de49bcb6b9e8c
The Rekrab <nolanb65244@gmail.com>
2022-07-29 12:02:37 -0400

    Added bank feature to keep track of winnings.

---
+++ bank.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFSIZE 2048
+
+long get_balance(char* username) {
+  char file_path[BUFFSIZE] = "Accounts/";
+  strncat(file_path, username, BUFFSIZE-1);
+  FILE *fp = fopen(file_path, "r");
+  if (fp == NULL) {
+    // Add the starting 100 coins.
+    FILE *new = fopen(file_path, "w");
+    fprintf(new, "100");
+    fclose(new);
+    return 100;
+  }
+  char coins_str[BUFFSIZE];
+  fgets(coins_str, BUFFSIZE, fp);
+  fclose(fp);
+  if ( !(atol(coins_str)) ) {
+    puts("Error reading coin balance.");
+    exit(0);
+  } else {
+    return atol(coins_str);
+  }
+}
+
+void add_coins(char* username, long gain) {
+  long current_balance = get_balance(username) + gain;
+  char file_path[BUFFSIZE] = "Accounts/";
+  strncat(file_path, username, BUFFSIZE-1);
+  FILE *fp = fopen(file_path, "w");
+  fprintf(fp, "%ld", current_balance);
+  fclose(fp);
+}
--- coinflip.c
+++ coinflip.c
@@ -4,16 +4,16 @@
 #include <string.h>
 #include <time.h>
 #include "ascii_art.h"
+#include "bank.h"
 
-#define START_MONEY 100
-#define BUFFSIZE 100
+#define BUFFSIZE 2048
 
-int flip();
+long flip();
 void rules();
 void clear_buffer();
-void loop(int* coins);
-int result_bet(int coins_to_bet, char* bet, char* correct);
-int place_bet(int coins);
+void loop(long *coins, char* username);
+long result_bet(long coins_to_bet, char* bet, char* correct);
+long place_bet(long coins);
 
 int main() {
   srand(time(NULL));
@@ -25,17 +25,23 @@ int main() {
   clear_buffer();
   if ( play == 'y' || play == 'Y' ) {
     rules();
-    int money = START_MONEY;
+    char username[BUFFSIZE];
+    printf("Enter your name:  ");
+    fgets(username, BUFFSIZE, stdin);
+    username[strcspn(username, "\n")] = 0;
+    long money = get_balance(username);
+    printf("You have %ld coins to start the round with.\n", money);
     while (1) {
-        loop(&money);
+        loop(&money, username);
     }
+
   } else {
       puts("Ok. Come back to the Coin Casino later...");
   }
 }
 
 // Basic stuff:
-int flip() {
+long flip() {
   return rand() % 2;
 }
 
@@ -46,30 +52,30 @@ void rules() {
   puts("    If you win the bet, you will gain however much money you bet.");
   puts("    However, if you loose, you lose however much money you bet.");
   puts(" 3. This continues until you decide to quit, or are out of money.");
-  printf("You start the game with exactly %d coins. Let's get this round started!\n\n", START_MONEY);
+  printf("You start the game with exactly 100 coins (if this is your first time). Let's get this round started!\n\n");
 }
 
 void clear_buffer() {
-  int c;
+  long c;
   while ( (c = getchar()) != '\n' && c != EOF) {
     ; // Do nothing...
   }
 }
 
 // More complex functions:
-int solve_bet(int coins_to_bet, char* bet, char* correct) {
+long solve_bet(long coins_to_bet, char* bet, char* correct) {
   printf("The coin was %s!\n", correct);
   // Did the user's bet win?
   if ( strcmp(bet, correct) == 0 ) {
-    printf("You won the bet! You win %d coins.\n", coins_to_bet);
+    printf("You won the bet! You win %ld coins.\n", coins_to_bet);
     return coins_to_bet;
   } else {
-    printf("Sorry, you did not win the bet. Better luck next time. You lose %d coins.", coins_to_bet);
+    printf("Sorry, you did not win the bet. Better luck next time. You lose %ld coins.", coins_to_bet);
     return 0 - coins_to_bet;
   }
 }
 
-int place_bet(int coins) {
+long place_bet(long coins) {
   // Get amount of money to bet:
   printf("Betting time!\nHow many coins do you want to bet?  ");
   char coins_to_bet_str[BUFFSIZE];
@@ -79,23 +85,23 @@ int place_bet(int coins) {
     puts("Not a number!");
     return 0;
   } else if (atoi(coins_to_bet_str) < 0) { // Prevent user from entering random numbers.
-    printf("You cannot bet %d coins. Try again.\n", atoi(coins_to_bet_str));
+    printf("You cannot bet %ld coins. Try again.\n", atoi(coins_to_bet_str));
     return 0;
   }
-  int coins_to_bet = atoi(coins_to_bet_str);
+  long coins_to_bet = atoi(coins_to_bet_str);
   // Check if it is OK to be removed from the account:
   if (coins_to_bet > coins) {
-    printf("You cannot spend that many coins. You only have %d coins.\n", coins);
+    printf("You cannot spend that many coins. You only have %ld coins.\n", coins);
     return 0;
   }
   // Get bet (heads or tails):
-  printf("You have wagered %d coins.\nWhat do you think the outcome will be? (heads/tails):  ", coins_to_bet);
+  printf("You have wagered %ld coins.\nWhat do you think the outcome will be? (heads/tails):  ", coins_to_bet);
   char bet[BUFFSIZE];
   fgets(bet, BUFFSIZE, stdin);
   bet[strcspn(bet, "\n")] = 0; // Replace \n with a null byte.
   // Flip coin
   puts("Flipping coin...");
-  int result = flip();
+  long result = flip();
   if (result) {
     char correct[] = "heads";
     return solve_bet(coins_to_bet, bet, correct);
@@ -106,22 +112,24 @@ int place_bet(int coins) {
 
 }
 
-void loop(int* coins) {
-  int gain = place_bet(*coins);
+void loop(long *coins, char* username) {
+  long gain = place_bet(*coins);
   if (gain == 0) {
     return; // Restart the loop
   }
   *coins += gain;
+  add_coins(username,gain);
   if (*coins == 0) {
     printf("OH, NO! You no longer have any coins. You are now broke, and must leave the casino.\nCome again soon with more money!\n\n\n");
     game_over();
     exit(0);
+
   } else {
-    printf("You currently have %d coins.\nWould you like to keep playing? (y/n)  ", *coins);
+    printf("You currently have %ld coins.\nWould you like to keep playing? (y/n)  ", *coins);
     char cont = getchar();
     clear_buffer();
     if (tolower(cont) != 'y') {
-      printf("You leave the table with %d coins.\nCome back to the Coin Casino to try and earn more than that next time.\n", *coins);
+      printf("You leave the table with %ld coins.\nCome back to the Coin Casino to try and earn more than that next time.\n", *coins);
       game_over();
       exit(0);
     }
